498be448316015af43a652d30c44eab2d8d9de6d
moekaba23 <moekaba23@gmail.com>
2021-07-25 23:40:27 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+let darkMode = localStorage.getItem('darkMode');
+const darkModeToggle = document.querySelector('.toggle input');
+
+// Check if dark mode is enableDarkMode
+// if enabled, turn it off
+// if disabled, turn it on
+
+const enableDarkMode = () => {
+  // 1. add the class dark-theme to the body
+  document.body.classList.add('dark-theme');
+  // 2. update darkMode in the LocalStorage
+  localStorage.setItem('darkMode', 'enabled');
+};
+
+const disableDarkMode = () => {
+  // 1. remove the class dark-theme to the body
+  document.body.classList.remove('dark-theme');
+  // 2. update darkMode in the LocalStorage
+  localStorage.setItem('darkMode', null);
+};
+
+if (darkMode === 'enabled') {
+  enableDarkMode();
+}
+
+darkModeToggle.addEventListener('click', () => {
+  darkMode = localStorage.getItem('darkMode');
+
+  if (darkMode !== 'enabled') {
+    enableDarkMode();
+    console.log(darkMode);
+  } else {
+    disableDarkMode();
+    console.log(darkMode);
+  }
+});
